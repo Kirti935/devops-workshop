aae225979644ed915d0cedd68c586873d6c781f5
Kirti Sinha <awskirti@gmail.com>
2025-07-24 14:34:14 +0530

    new file

--- terraform_code/V1-EC2.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-    region = "ap-south-1"
-}
-resource "aws_instance" "demo-server" {
-    ami = "ami-0b32d400456908bf9"
-    instance_type = "t2.micro"
-    key_name = "devops-key"
-}
--- terraform_code/V4-EC2-With_VPC_for_each.tf
+++ terraform_code/V4-EC2-With_VPC_for_each.tf
@@ -2,31 +2,46 @@ provider "aws" {
     region = "ap-south-1"
 }
 resource "aws_instance" "demo-server" {
-    ami = "ami-0b32d400456908bf9"
+    ami = "ami-0f918f7e67a3323f0"
     instance_type = "t2.micro"
     key_name = "devops-key"
     //security_groups = ["demo-sg"]
     subnet_id = aws_subnet.demo-public-subnet-01.id
     vpc_security_group_ids = [aws_security_group.demo-sg.id]
-    
+    for_each = toset(["jenkins-master", "build-slave", "ansible"])
+   tags = {
+     Name = "${each.key}"
+   } 
 }
 
 resource "aws_security_group" "demo-sg" {
     name        = "demo-sg"
     description = "SSH acecess to demo server"
     vpc_id     = aws_vpc.demo-vpc.id
+
     ingress {
         description = "SSH access"
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
+        
     }
+
+    ingress {
+    description      = "Jenkins port"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
     egress {
         from_port   = 0
         to_port     = 0
         protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
     }
     tags = {
         Name = "ssh-port"
